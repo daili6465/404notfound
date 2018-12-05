cfae98027f609ee8707db4cf1bbe0bf67c35f08a
Castie2 <43663931+Castie2@users.noreply.github.com>
2018-12-05 09:28:25 -0800

    Add files via upload
    Added HTML files for animation
    Fixed my code.

--- FINALPROJECT.R
+++ FINALPROJECT.R
@@ -1,24 +1,18 @@
 Pet_Data <- read.csv("~/Documents/Seattle_Pet_Licenses.csv", stringsAsFactors = FALSE)
 View(Pet_Data)
 library(dplyr)
-Only_Date <- select(Pet_Data, License.Issue.Date, Species, Primary.Breed, Secondary.Breed)
+Only_Date <- select(Pet_Data, License.Issue.Date, Species)
 View(Only_Date)
 library(lubridate)
-mm/dd/yyy
 library(ggplot2)
 library(tidyr)
 
-data <- readRDS("~/Documents/Seattle_Pet_Licenses.csv", stringsAsFactors = FALSE)
-data$Region <- as.factor(data$Region)
-
+#Making month/ year stuff
 Only_Date$date <- lubridate::mdy(Only_Date$License.Issue.Date)
 Only_Date$month <- lubridate::month(Only_Date$date)
-Only_Date$dayofwk <- lubridate::wday(Only_Date$date)
-Only_Date$dayofmonth <- lubridate::day(Only_Date$date)
 Only_Date$Month <- lubridate::month(Only_Date$date, label=TRUE)
-Only_Date$Weekday <- lubridate::wday(Only_Date$date, label=TRUE)        
 Only_Date$Year <- lubridate::year(Only_Date$date)
-
+View(Only_Date)
 #x = time, y = pets/population 
 
 C <- subset(Only_Date, Species == "Cat")
@@ -45,7 +39,7 @@ G2018$Population <- 13
 C2003 <- subset(C, Year == "2003")
 C2003$Population <- 1
 C2006 <- subset(C, Year == "2006")
-C2006$Population <- 2
+C2006$Population <- 1
 C2014 <- subset(C, Year == "2014")
 C2014$Population <- 4
 C2015 <- subset(C, Year == "2015")
@@ -79,7 +73,7 @@ D2018$Population <- 18188
 # Added Populations together for all years
 Population_Goat <- rbind.data.frame(G2016, G2017, G2018)
 Population_Cat <- rbind.data.frame(C2003, C2006, C2014,C2015,C2016, C2017, C2018)
-Population_Dog <- rbind.data.frame(D2003, D2004, D2008, D2011, D2012, D2014,
+Population_Dog <- rbind.data.frame(D2004, D2008, D2011, D2012, D2014,
                                    D2015, D2016, D2017, D2018)
 #Pets
 Population_Pets <- rbind.data.frame(Population_Cat, Population_Dog, Population_Goat, P)
@@ -88,20 +82,20 @@ install.packages("gapminder")
 if (!require(devtools))
   install.packages("cowplot")
 devtools::install_github("dgrtwo/gganimate")
+
 library(gapminder)
 library(gganimate)
+install.packages("plotly")
+library(plotly)
 
 Population_Pets$id <- seq.int(nrow(Population_Pets))
-
+View(Population_Pets)
 # Create the plot
-Pet_Plot <- ggplot(Population_Pets, aes(Year, Population, size = Population, color = Species, frame = Month)) +
-  geom_point()+ 
-  labs(x="Month", y = "Population of Pets", color = 'Species',size = "Population") + 
+Pet_Plot <- ggplot(Population_Pets, aes(Year, Population, size = Population, color = Species, frame = Month)) + geom_point()+ 
+  labs(x="Year", y = "Population of Pets Licenses", color = 'Species',size = "Population") + 
   scale_color_brewer(type = 'div', palette = 'Spectral')
 
-Animation <- ggplotly(Pet_Plot) %>%
-  animation_opts(frame = 12,
-                 easing = "linear",
-                 redraw = FALSE)
+Animation <- ggplotly(Pet_Plot)
 
-
+Animation
+htmlwidgets::saveWidget(Animation, "Plot.html")
